01df96cb949fc4da8ac0d54a200a2cb69478b94b
Joseph Shook <Joseph.Shook@Surescripts.com>
2024-03-22 14:31:09 -0700

    Switched back from per/page to global render mode.
    This gets my dynamic popover menu behavior working again when working with non Static Server side pages.  Specifically the "Individual Accounts' feature from MS that contains all the login and user account management.  This was supposed to make life easy, but being Server Side static it doesn't play well with MudBlazor.  Kind of a crappy experience over the last few days.

--- Fhirlabs.Ehr.Client/Program.cs
+++ Fhirlabs.Ehr.Client/Program.cs
@@ -7,6 +7,7 @@
 // */
 #endregion
 
+using Blazored.LocalStorage;
 using Fhirlabs.Ehr.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -20,5 +21,6 @@ builder.Services.AddMudServices();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
